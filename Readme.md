f46b2a07de263ecf3f69ab3fe41b6aaa537066c4
Abhishek <av1950@benntt.edu>
2024-12-16 07:52:27 -0600

    Basics of react

--- Readme.md
+++ Readme.md
@@ -1,1 +1,23 @@
 # Learning React from Scratch
+
+# Parcel
+.Dev build
+Local Server
+Automatically refreshing the page=>HMR(Hard Module Replacement)
+Parcel uses the file watching algorithm and written in c++
+Caching-Faster builds
+Image optimization
+Minification of file
+Bundling
+compress the files
+Consistent hashing
+code splitting
+Differential bundling-It makes app run smoothly in th older browsers as well
+Diagnostics behind the scene
+Error Handling
+Helps in host the website in https
+Tree Shaking algorithm- Removes unused code for you
+Different bundlers for dev and prod
+
+
+Read parcel documentation for more  info
--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 //Always returns the object
 //ReactElement(Object)=>HTML(Browser Understands)
-const parent = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', {}, 'I am h1 tag'),
-    React.createElement('h2', {}, 'I am h2 tag'),
-  ]),
-  ,
-  React.createElement('div', { id: 'child2' }, [
-    React.createElement('h3', {}, 'I am h3 tag'),
-    React.createElement('h4', {}, 'I am h4 tag'),
-  ]),
-]);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// the render basically takes the object and convert it into  h1 tag and put it up on the dom
+
+//React createElement=>JS Object=>When we render this obj on to the DOM it becomes the html element
+
+//JSXelement=>Transpile to to React element that is React.createElement=>ReactElement=>JSObject=>HTML element(Rendering)
+//babel is converting JSX to React.createElement
+
+const elem = <span>This is React element</span>;
+
+//React Functional Components
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Hello there, Abhishek
+  </h1>
+);
+
+//React Component
+const title2 = (
+  <h1 className="head" tabIndex="5">
+    {elem}
+    Hello there, This is React Component
+  </h1>
+);
+
+//React Functional Components=> This can be defined as a function that returns the JSX Code/React element that become the Functional Component
+
+const HeadingComponent1 = () => (
+  <div>
+    {title2}
+    {<Title />}
+    <h1 className="heading">This line is React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent); // this render basically takes the object and convert it into  h1 tag and put it up on the dom
+root.render(<HeadingComponent1 />); //Rendering
+
+
+
+
